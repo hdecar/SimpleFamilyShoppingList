353409e79d1eef31b21855b9e7006cd5d105f6fe
Holly Decardenas <hollydecardenas@gmail.com>
2019-08-24 21:59:11 -0400

    Added jwt tokens to list API

--- NodeJS/api/routes/list.js
+++ NodeJS/api/routes/list.js
@@ -2,88 +2,135 @@ const express = require('express');
 const router = express.Router();
 const List = require('../models/list');
 const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+
+function tokenIsValid(request, next){
+    const token = request.headers["token"];
+    jwt.verify(token, process.env.SECRET_KEY, next);
+}
 
 //get the current list
 router.get('/:id', (request, response)=>{
-    List.findOne({'_id': request.params.id}, (err, list) => {
-        if (err || !list){
-            response.status(403).json({message: "List not found."});
-            return;
+    tokenIsValid(request, 
+        (err)=>{
+            if (err){
+                response.status(403).json({message: "Expired session."});
+                return;
+            }
+
+            List.findOne({'_id': request.params.id}, (err, list) => {
+                if (err || !list){
+                    response.status(403).json({message: "List not found."});
+                    return;
+                }
+                response.status(200).json(list);
+                return;
+            });
         }
-        //TODO: return JWT token with result
-        response.status(200).json(list);
-    });
+    );
 });
 
 //add one item to the list
 router.post('/:id', (request, response)=>{
-    List.findOne({'_id': request.params.id}, (err, list) => {
-        if (err || !list){
-            response.status(403).json({message: "List not found."});
-            return;
-        }
-        //TODO: return JWT token with result
-        const result = validateItem(request.body);
-        if (result.error) return response.status(400).send(result.error.details[0].message);
 
-        const item = request.body.item;
+    tokenIsValid(request, 
+        (err)=>{
+            if (err){
+                response.status(403).json({message: "Expired session."});
+                return;
+            }
 
-        const itemToAdd = {
-            id: list.items.length + 1,
-            item: item
-        }
+        List.findOne({'_id': request.params.id}, (err, list) => {
+            if (err || !list){
+                response.status(403).json({message: "List not found."});
+                return;
+            }
+            
+            const result = validateItem(request.body);
+            if (result.error) return response.status(400).send(result.error.details[0].message);
 
-        list.items.push(itemToAdd);
+            const item = request.body.item;
 
-        list.save()
-        .then(result => {
-            console.log(result);
-            response.send(list);
-        })
-        .catch(err => {console.log(err);});
+            const itemToAdd = {
+                id: list.items.length + 1,
+                item: item
+            }
+
+            list.items.push(itemToAdd);
+
+            list.save()
+            .then(result => {
+                response.send(list);
+            })
+            .catch(err => {console.log(err);});
+        });
     });
 });
 
 //delete one item from the list
 router.delete('/:id/:itemid', (request, response)=>{
-    List.findOne({'_id': request.params.id}, (err, list) => {
-        if (err || !list){
-            response.status(403).json({message: "List not found."});
-            return;
-        }
-        const itemid = parseInt(request.params.itemid);
-        const item = list.items.find(x=>x.id === itemid);
 
-        if (!list) return response.status(404).send('Item not found');
-        
-        const index = list.items.indexOf(item);
-        list.items.splice(index, 1);
+    tokenIsValid(request, 
+        (err)=>{
+            if (err){
+                response.status(403).json({message: "Expired session."});
+                return;
+            }
 
-        list.save()
-        .then(result => {
-            console.log(result);
-            response.send(list);
-        })
-        .catch(err => {console.log(err);});
+        List.findOne({'_id': request.params.id}, (err, list) => {
+            if (err || !list){
+                response.status(403).json({message: "List not found."});
+                return;
+            }
+            const itemid = request.params.itemid;
+            
+            //TODO: fix the following query
+            List.findOne({'items': {'_id': itemid}}, (err, item)=>{
+
+                if (err || !item) {
+                    console.log(err);
+                    response.status(404).send('Item not found');
+                    return;
+                }
+                
+                const index = list.items.indexOf(item[0]);
+                list.items.splice(index, 1);
+
+                list.save()
+                .then(result => {
+                    console.log(result);
+                    response.send(list);
+                })
+                .catch(err => {console.log(err);});
+            });
+
+        });
     });
 });
 
 //clear the list
 router.delete('/:id', (request, response)=>{
-    List.findOne({'_id': request.params.id}, (err, list) => {
-        if (err || !list){
-            response.status(403).json({message: "List not found."});
-            return;
-        }
-        
-        list.items = [];
+    tokenIsValid(request, 
+        (err)=>{
+            if (err){
+                response.status(403).json({message: "Expired session."});
+                return;
+            }
 
-        list.save()
-        .then(result => {
-            console.log(result);
-            response.send(list);
-        })
-        .catch(err => {console.log(err);});
+        List.findOne({'_id': request.params.id}, (err, list) => {
+            if (err || !list){
+                response.status(403).json({message: "List not found."});
+                return;
+            }
+            
+            list.items = [];
+
+            list.save()
+            .then(result => {
+                response.send(list);
+            })
+            .catch(err => {console.log(err);});
+        });
     });
 });
 
